82a85e099820c887ca05001aa83e4b8ff1da7927
Byron <byron303@gmail.com>
2025-01-11 12:34:20 -0600

    updating Meta data and prices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,9 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Blvck Sheep Shuttles | Premium Shuttle Services in Guatemala",
+  title: "Blvck Sheep Shuttles | Transportation Services in Guatemala",
   description:
-    "Discover Guatemala with Blvck Sheep Shuttles. Offering exclusive, reliable, and comfortable transportation to top destinations like Antigua, Lake Atitlan, Lanquin, and El Paredon. Your journey, your way.",
+    "Explore Guatemala in comfort with Blvck Sheep. Offering a more exclusive, reliable, and comfortable transportation to top destinations like Antigua, Lake Atitlan, Lanquin, and El Paredon. Travel differently.",
   keywords:
     "Guatemala shuttles, Black Sheep Shuttles, transportation in Guatemala, private shuttles Guatemala, shared shuttles Guatemala, Antigua to Lake Atitlan, Guatemala City transport, El Paredon shuttles, premium travel Guatemala, tourist transportation Guatemala",
 };
--- src/components/FeatureAntigua.tsx
+++ src/components/FeatureAntigua.tsx
@@ -7,7 +7,7 @@ const FeatureAntigua = () => {
     <section className="group inline-block overflow-hidden rounded-xl">
       <BlogCardFeat
         title="Antigua Guatemala"
-        description="Prices per passenger starting at: Pana-Antigua: $20 | Guate-Antigua: $15"
+        description="Prices per passenger starting at: Pana-Antigua: $18 | Guate-Antigua: $15"
         className="col-span-2 lg:col-span-1"
       >
         <div className="aspect-video flex items-center justify-center overflow-hidden rounded-3xl">
